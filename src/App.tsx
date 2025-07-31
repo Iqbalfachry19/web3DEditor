3917a598f8e1310b541dc6e39ee98ea6bd05de33
iqbal <ifachry@rocketmail.com>
2025-07-31 14:11:56 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { MainMenu } from "./MainMenu";
 import { Editor } from "./Editor"; // your actual 3D editor
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   const [started, setStarted] = useState(false);
 
-  return started ? (
-    <Editor />
-  ) : (
-    <MainMenu onProjectLoaded={() => setStarted(true)} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {started ? (
+          <Route path="/" element={<Editor />} />
+        ) : (
+          <Route
+            path="/"
+            element={<MainMenu onProjectLoaded={() => setStarted(true)} />}
+          />
+        )}
+        <Route path="/project" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
